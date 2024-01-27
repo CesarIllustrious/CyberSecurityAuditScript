6fb0210a75b9bfb63e6ad6777ad79458ebd04df3
CesarIllustrious <110250079+CesarIllustrious@users.noreply.github.com>
2024-01-27 13:34:38 +0000

    Functions SASv2.py
    Removed a lot of DRY code, added functions, cleaned up everything, added private FW status

--- SASv1.py
+++ SASv1.py
@@ -1,55 +1,80 @@
-import os, sys, winapps, subprocess, wmi
-from unittest import result
- 
+import os
+import winapps
+import subprocess
+
+outputfile = 'output.txt'  #output file
+
+#Labels and formats
+Start_label = '=== is new section,\n--- is new command in the section\n### is info type\n\n===============================================================================\n               ############## AV&VPN ##############\n==============================================================================='
+Label_format = '===============================================================================\n               ############## {} ##############\n===============================================================================\n'
+CMD_Break = '-------------------------------------------------------------------------------\n'
+
+#Label Variables
+Users = 'Users'
+RDP = 'Remote Connections'
+anydesk = 'Anydesk'
+TV = 'Team Viewer'
+AV = 'Anti Virus Status'
+FW = 'Firewall Status'
+Sinfo = 'System Info'
+IP = 'IP Config'
+BL = 'Bit Locker'
+SRV = 'Services'
+SFT = 'Software'
+AVname = 'AV Example'  #Change the variable to your AV name
+VPN_NAME = 'VPN Example' #Change the variable to your VPN name
 
 
-with open('output.txt', 'w') as f:
-#### AV & VPN Check
-    print('=== is new section,\n--- is new command in the section\n### is info type\n\n===============================================================================\n               ############## AV&VPN ##############\n===============================================================================', end='\n\n', file=f)    
-    AV = list(winapps.search_installed('NAME OF ANTIVIRUS'))   # <---- change the name to your company's AV
-    if AV: print(*AV,sep='\n\n', end='\n\n', file=f)
-    else: f.write("-----------------------------------\n|!!!!! NAME OF AV not found !!!!!|\n-----------------------------------\n"), f.flush   # <---- change the output name
-    
-    VPN = list(winapps.search_installed('NAME OF VPN'))  # <---- change to company VPN name
-    if VPN: print(*VPN, sep='\n\n', end='\n\n', file=f)
-    else: f.write("----------------------------------------\n|!!!!! NAME OF VPN not found !!!!!|\n----------------------------------------\n===============================================================================\n               ############## User Accounts ##############\n==============================================================================="), f.flush()
-                                                                      # ^ change the output name
-#### Users check   
-    p1 = subprocess.run(["powershell.exe", """net user"""], shell=True, stdout=f, text=True)   #show all users    
-    print("-------------------------------------------------------------------------------\n               ############## Admin accounts ##############\n-------------------------------------------------------------------------------", end='\n\n', file=f), f.flush()     
-    
-    p2 = subprocess.run(["powershell.exe", """net localgroup administrators"""], shell=True, stdout=f, text=True)    #show admin users
-    print('===============================================================================\n               ############## Remote Connections ##############\n===============================================================================', file=f), f.flush()    
- #####  Remote connections check
-    p3 = subprocess.run(["powershell.exe", "-Command", 'get-service "remote desktop services" | select Displayname,Status,ServiceName,Can*'], shell=True, stdout=f, text=True),  f.flush
-   
-    AD = list(winapps.search_installed('Anydesk'))
-    if AD: print(*AD, sep='\n\n', end='\n\n', file=f)
-    else: f.write("---------------------\n| Anydesk not found |\n---------------------\n"), f.flush()               
+#PowerShell Commands Variables
+NUCMD = 'net user'
+AdminCMD = 'net localgroup administrators'
+RDPCMD = 'get-service "remote desktop services" | select Displayname,Status,ServiceName,Can*'
+AVCMD = 'Get-MpComputerStatus'
+SinfoCMD = 'systeminfo'
+FWCMD = 'netsh advfirewall show Publicprofile'
+FWCMD2 = 'netsh advfirewall show privateprofile'
+IPCMD = 'ipconfig /all'
+BLCMD = 'manage-bde -status'
+SRVCMD = """Get-Service | Select StartType, Status, Name, DisplayName | Where-Object {$_.Status -eq 'Running'} | Format-Table -AutoSize"""
 
-    TV = list(winapps.search_installed('TeamViewer'))
-    if TV: print(*TV, sep='\n\n', end='\n\n', file=f)
-    else: f.write("-------------------------\n| TeamViewer not found  |\n-------------------------\n")
-    print('===============================================================================\n               ############## AntiVirus Status ##############\n===============================================================================', file=f), f.flush()            
-#### Status of antimalware, sysinfo, services, firewall settings, bitlocker status
-    p4 = subprocess.call('powershell.exe Get-MpComputerStatus', shell=True, stdout=f, text=True)    #AV Detail status
-    print("===============================================================================\n               ############## SYSTEMINFO ##############\n===============================================================================", file=f), f.flush() 
+
+
+#Label Format Function
+def formatlabel(variable):
      
-    p5 = subprocess.run('systeminfo', stdout=f, text=True)            #sysinfo    
-    print("\n===============================================================================\n               ############## FIREWALL-STATUS ##############\n===============================================================================", file=f), f.flush() 
+        return Label_format.format(variable)
+
+
+
+#Write Function
+def write(text):
+    with open(outputfile, 'a') as f:
+        f.write(text)
+        f.flush()
+
+#Software Search Function
+def search (name):
+    search1 =  list(winapps.search_installed(name))
+    if search1:
+        write(f"\n {name} is installed\n")
+    else:
+        write(f"\n-----------------------------------\n|!!!!! {name} not found !!!!!|\n-----------------------------------\n") 
+
+
+
+
+
+
+
+#Run PowerShellCMD Function
+def run_PSCMD(input):
     
-    p6 = subprocess.check_call('netsh advfirewall show Publicprofile', stdout=f, text=True)  # FWINFO                         
-    print("===============================================================================\n               ############## IPCONFIG ##############\n===============================================================================", file=f), f.flush() 
-   
-    p7 = subprocess.run('ipconfig /all', stdout=f, text=True)  # IPINFO            
-    print('\n\n===============================================================================\n               ############## BITLOCKER-STATUS ##############\n===============================================================================', end='\n\n', file=f), f.flush() 
+        test = subprocess.run(['powershell.exe',input], shell=True, stdout=subprocess.PIPE, text=True)
+        
+        write(test.stdout)
 
-    p8 = subprocess.run(["powershell.exe", """manage-bde -status"""], shell=True, stdout=f, text=True)
-    print("\n\n===============================================================================\n                  ############## SERVICES ##############\n===============================================================================", file=f), f.flush() 
-
-    p9 = subprocess.run(["powershell.exe", """Get-Service | Select StartType, Status, Name, DisplayName | Where-Object {$_.Status -eq 'Running'} | Format-Table -AutoSize"""], shell=True, stdout=f, text=True)  # running services
-    print("===============================================================================\n               ############## INSTALLED-SOFTWARE ##############\n===============================================================================", file=f), f.flush() 
-##### Installed software check  & extra scans featureS
+#Installed Software Format Function
+def installed_software():
     output = subprocess.run(
     ["powershell.exe", "-Command", 'wmic product get name'],
     shell = True,
@@ -60,31 +85,65 @@ with open('output.txt', 'w') as f:
     # Write only the non-empty lines to the file
     for line in lines:
         if line.strip():
-            f.write(line + "\n")                       
-    f.flush()
-    #FEATURE ENABLED BY DELETING '#' before the px, requires admin
+            write(line + "\n")               
+
+#The Script Main 
+def main():
+    
+    write(Start_label)
+
+    search(AVname)
+    search(VPN_NAME)
+
+    write(formatlabel(Users))
+
+    run_PSCMD(NUCMD)
+
+    write(CMD_Break)
+
+    run_PSCMD(AdminCMD)
+    
+
+    write(formatlabel(RDP))
+    run_PSCMD(RDPCMD)
+    write(CMD_Break)
+    search(anydesk)
+    write(CMD_Break)
+    search(TV)
+
+    write(formatlabel(AV))
+    run_PSCMD(AVCMD)
+
+    write(formatlabel(Sinfo))
+    run_PSCMD(SinfoCMD)
+
+    write(formatlabel(FW))
+    run_PSCMD(FWCMD)
+    write(CMD_Break)
+    run_PSCMD(FWCMD2)
+
+    write(formatlabel(IP))
+    run_PSCMD(IPCMD)
+    
+    write(formatlabel(BL))
+    run_PSCMD(BLCMD)
+
+    write(formatlabel(SRV))
+    run_PSCMD(SRVCMD)
+
+    write(formatlabel(SFT))
+    installed_software()
+
+
+
+if __name__ == "__main__":
+     main()
+
+
+# Star the file automatically
+os.startfile(outputfile)
+
+
+#FEATURE ENABLED BY DELETING '#' before the px, requires admin
     #p6 = subprocess.run('sfc /scannow', stdout=f, text=True)  # System File Checker in case new drives needed   
     #p7 = subprocess.call('powershell.exe Start-MpScan', shell=True, stdout=f, text=True)    # start AV scan
-   
-os.startfile('output.txt')   # Open the output text file
-
-
-    
-    
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
